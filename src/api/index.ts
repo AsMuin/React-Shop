e5a4ec580351649989be7bcdc096c6f40a106341
AsMuin <asmuins@foxmail.com>
2024-11-08 01:51:53 +0800

    update

---
+++ src/api/index.ts
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import type { AxiosResponse, AxiosRequestConfig } from 'axios';
+import { toast } from 'react-toastify';
+interface IData<T = any> {
+    success: boolean;
+    message: string;
+    data?: T;
+    token?: string;
+}
+const baseURL = '/api';
+const Axios = axios.create({
+    baseURL
+});
+Axios.interceptors.request.use(
+    config => {
+        if (config.url === '/user.login') {
+            return config;
+        } else {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                toast.warn('请先登录', { progress: 1 }),
+                    setTimeout(() => {
+                        window.location.href = '/login';
+                    }, 2500);
+                return Promise.reject(new Error('请先登录'));
+            } else {
+                config.headers.Authorization = token;
+                return config;
+            }
+        }
+    },
+    error => {
+        toast.error(error);
+        return Promise.reject(error);
+    }
+);
+Axios.interceptors.response.use(
+    (response: AxiosResponse<IData, any>) => {
+        const { data } = response;
+        if (data.success) {
+            return response;
+        } else {
+            toast.error(data.message);
+            return Promise.reject(data.message);
+        }
+    },
+    error => {
+        toast.error(error.message);
+        return Promise.reject(error);
+    }
+);
+async function request<T = any>(config: AxiosRequestConfig): Promise<IData<T>> {
+    const Response = await Axios.request<IData<T>>(config);
+    return Response.data;
+}
+export default request;
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,34 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, './src')
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const common = {
+        plugins: [react()],
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, './src')
+            }
         }
+    };
+    if (command === 'serve') {
+        return {
+            ...common,
+            // dev 独有配置
+            server: {
+                proxy: {
+                    '/api': {
+                        target: env.VITE_BACKEND_URL,
+                        changeOrigin: true
+                    }
+                }
+            }
+        };
+    } else {
+        // command === 'build'
+        return {
+            ...common
+            // build 独有配置
+        };
     }
 });
